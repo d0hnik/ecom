3c319bd4e7ceb27d1b53a0e99067c487369d0896
d0hnik <crazylightdanik@gmail.com>
2024-07-29 15:29:32 +0300

    Added search, user profile and much more

--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
+from django.contrib import messages
 
 
 def cart_summary(request):
@@ -27,6 +28,7 @@ def cart_add(request):
         cart_quantity = cart.__len__()
         # response = JsonResponse({'Product Name: ': product.name})
         response = JsonResponse({'qty': cart_quantity})
+        messages.success(request, 'Product added to cart')
         return response
 
 
@@ -37,6 +39,7 @@ def cart_delete(request):
 
         cart.delete(product_id=product_id)
         response = JsonResponse({'message': "success"})
+        messages.success(request, 'Product deleted successfully')
         return response
 
 
@@ -50,4 +53,5 @@ def cart_update(request):
         else:
             cart.delete(product_id=product_id)
         response = JsonResponse({'qty': cart.__len__()})
+        messages.success(request, 'Cart updated successfully')
         return response
--- ecom/store/urls.py
+++ ecom/store/urls.py
@@ -7,7 +7,11 @@ urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
+    path('update_user/', views.update_user, name='update_user'),
+    path('update_info/', views.update_info, name='update_info'),
+    path('update_password/', views.update_password, name='update_password'),
     path('product/<int:pk>', views.product, name='product'),
     path('category/<str:foo>', views.category, name='category'),
-    path('card', views.card, name='card'),
+    path('search/', views.search, name='search'),
+
 ]
